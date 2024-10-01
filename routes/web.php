52e70abb0e20be0369dba49fe4aa20b7c770befc
Mohamed Elsayed <mohamdeesayed@outlook.com>
2024-10-01 21:20:29 +0300

    add system security

--- routes/web.php
+++ routes/web.php
@@ -12,25 +12,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WebAuthController::class, 'login'])->name('dashboard.login'); // update system to be make login is the index page
 
-Route::prefix('Team')->group(function(){
-Route::get('all',[WebTeamController::class,'index'])->name('Team.index');
-Route::get('create',[WebTeamController::class,'create'])->name('Team.create');
-Route::post('store',[WebTeamController::class,'store'])->name('Team.store');
-Route::get('{id}/show',[WebTeamController::class,'show'])->name('Team.show');
-Route::delete('{id}/delete',[WebTeamController::class,'destroy'])->name('Team.destroy');
-Route::get('{id}/edit',[WebTeamController::class,'edit'])->name('Team.edit');
-Route::put('{id}/update',[WebTeamController::class,'update'])->name('Team.update');
+
+Route::prefix('dashboard')->group(function(){
+    Route::middleware('auth')->group(function(){
+        //app system log out
+        
+        //app utlites controllers 
+        Route::prefix('Team')->group(function(){
+            Route::get('all',[WebTeamController::class,'index'])->name('Team.index');
+            Route::get('create',[WebTeamController::class,'create'])->name('Team.create');
+            Route::post('store',[WebTeamController::class,'store'])->name('Team.store');
+            Route::get('{id}/show',[WebTeamController::class,'show'])->name('Team.show');
+            Route::delete('{id}/delete',[WebTeamController::class,'destroy'])->name('Team.destroy');
+            Route::get('{id}/edit',[WebTeamController::class,'edit'])->name('Team.edit');
+            Route::put('{id}/update',[WebTeamController::class,'update'])->name('Team.update');
+            });
+            
+            Route::prefix('Achievement')->group(function(){
+                Route::get('all',[WebAchievementController::class,'index'])->name('Achievement.index');
+                Route::get('create',[WebAchievementController::class,'create'])->name('Achievement.create');
+                Route::post('store',[WebAchievementController::class,'store'])->name('Achievement.store');
+                Route::get('{id}/show',[WebAchievementController::class,'show'])->name('Achievement.show');
+                Route::delete('{id}/delete',[WebAchievementController::class,'destroy'])->name('Achievement.destroy');
+                Route::get('{id}/edit',[WebAchievementController::class,'edit'])->name('Achievement.edit');
+                Route::put('{id}/update',[WebAchievementController::class,'update'])->name('Achievement.update');
+            });
+    });
 });
 
-Route::prefix('Achievement')->group(function(){
-    Route::get('all',[WebAchievementController::class,'index'])->name('Achievement.index');
-    Route::get('create',[WebAchievementController::class,'create'])->name('Achievement.create');
-    Route::post('store',[WebAchievementController::class,'store'])->name('Achievement.store');
-    Route::get('{id}/show',[WebAchievementController::class,'show'])->name('Achievement.show');
-    Route::delete('{id}/delete',[WebAchievementController::class,'destroy'])->name('Achievement.destroy');
-    Route::get('{id}/edit',[WebAchievementController::class,'edit'])->name('Achievement.edit');
-    Route::put('{id}/update',[WebAchievementController::class,'update'])->name('Achievement.update');
-});
     
 
 
